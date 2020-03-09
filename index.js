08574cf3e30e6a0f54f2abb637eb66af0973448b
Phillip <phillip.d.shields1382@gmaIl.com>
2020-03-10 11:13:24 +1300

    added equality symbols

--- index.js
+++ index.js
@@ -37,5 +37,24 @@ let b = "David";
 let c = "Shields";
 
 console.log("My name is " + a + " " + b + " " + c + ".");
-console.log(`${a} ${b} ${c}`);
+console.log(`My name is ${a} ${b} ${c}`);
+
+console.log(10 + "5");
+console.log("25" - 20);
+
+// equality check without type conversion
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(1 == true);
+console.log(1 ===true);
+console.log(0 == false);
+console.log(0 === false);
+
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
 
