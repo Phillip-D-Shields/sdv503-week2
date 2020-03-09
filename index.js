96bc88fa1893c70e22e034eb1fcaed0c7afa2c10
Phillip <phillip.d.shields1382@gmaIl.com>
2020-03-10 11:25:03 +1300

    OR examples

--- index.js
+++ index.js
@@ -50,11 +50,29 @@ console.log(1 ===true);
 console.log(0 == false);
 console.log(0 === false);
 
-
+// || OR examples
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log(1 || 1);
+console.log(1 || 0);
+console.log(0 || 1);
+console.log(0 || 0);
+console.log(true || 0);
+console.log(1 || false);
+console.log(true || 1);
+console.log(false || 1);
+console.log(false || 0);
+console.log(0 || 0);
 console.log();
 console.log();
 console.log();
 console.log();
 console.log();
 console.log();
-
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
