49c1073ca3ef7a8bcb90dac5f3d406dfac288e5c
Phillip <phillip.d.shields1382@gmaIl.com>
2020-03-09 13:49:42 +1300

    pre increment vs post increment

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 let x = 10;
 let y = 7;
+let z = 25;
 
+
+// basic arithmetic operators
 console.log( x + y );
 console.log( x - y );
 console.log( x * y );
 console.log( x / y );
 console.log( x % y );
 console.log( x ** y );
+
+
+// pre increment && pre decrease
+console.log(x);
+console.log(++x);
+console.log(++x);
+console.log(++x);
+console.log(--x);
+console.log(--x);
+console.log(--x);
+
+
+// post increment 
+console.log(z++);
+console.log(z++);
+console.log(z++);
+console.log(z--);
+console.log(z--);
+console.log(z--);
+console.log(z);
+
